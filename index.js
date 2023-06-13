f52757b2b59f26d896fda903e19125637ac1a7b1
Taha <tahaozcilingir@msn.com>
2023-06-12 22:14:40 -0400

    uses operators to do complex tasks

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
 
-// Write your code here
+const random = Math.floor(Math.random() * 10) + 1
+
+const num3 = 18
+const num4 = 7
+
+const mod = num3 % num4
+
+const numbers =[1,2,10,15,19,11,20,18]
+const max = Math.max(...numbers)
